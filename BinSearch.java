8113a2de81ebb4a02c00aa10c81bc697949ce1c2
MIN <minkyoon97@hanmail.net>
2023-01-04 15:59:00 +0900

    add algorizmstudy

---
+++ BinSearch.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class BinSearch {
+    //요솟수가 n인 배열 a에서 key와 같은 요소를 이진 검색
+    static int binSearch(int [] a, int n, int key) {
+        int pl = 0;
+        int pr = n - 1;
+
+
+        do {
+            int pc = (pl + pr) / 2;
+            if (a[pc] == key)
+                return pc;
+            else if (a[pc] < key)
+                pl = pc + 1;
+            else
+                pr = pc - 1;
+        } while (pl <= pr);
+
+        return -1; // 검색실패
+    }
+
+    public static void main(String[] args){
+        Scanner stdIn= new Scanner(System.in);
+
+        System.out.print("요솟수:");
+        int num= stdIn.nextInt();
+        int[] x = new int[num];
+
+        System.out.print("x[0:");
+        x[0] =stdIn.nextInt();
+
+        for(int i=1; i<num; i++){
+            do{
+                System.out.print("x["+i+ "]:");
+                x[i] = stdIn.nextInt();
+            }while(x[i] < x[i-1]);
+        }
+
+        System.out.print("검색할 값:");
+        int ky = stdIn.nextInt();
+
+        int idx = binSearch(x, num, ky);
+
+        if(idx==-1)
+            System.out.println("그 값의 요소가 없습니다.");
+        else
+            System.out.println(ky+"은(는) x["+ idx + "]에 있습니다.");
+
+
+        }
+    }
+
+
+
+
